b12ac73d9d79ff7ccaf9fb37decd98d4a7fd3c2b
thechitrakars <anil@thechitrakars.com>
2017-04-10 20:16:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var cookieParser = require('cookie-parser');
+var passport =require('passport');
+var session = require('express-session');
+
+//session and cookie management middleware
+
+app.use(cookieParser());
+app.use(session({secret: 'library'}));
+
+require('./config/passport')(app);
 
 // data/body parser
 var bodyParser = require('body-parser');
-app.use(bodyParser.json({limit: "50mb"}));
-app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:100000}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit:100000}));
 
 // routing
 var routes = require('./routes/index'); 
+var authRouter = require('./routes/authRoutes')();
 
+
+var router = express.Router();
 // define default control to check routes
+//console.log(authRouter);
+app.use('/auth', authRouter);
 app.use('/',routes);
 
 // set view engine to ejs
@@ -24,6 +39,7 @@ app.get('/', function(req, res){
   res.render('pages/index');
 });
 
+
 // About route
 app.get('/about', function(req, res){
   res.render('pages/about');
@@ -31,9 +47,8 @@ app.get('/about', function(req, res){
 
 // Status route
 app.get('/status', function(req, res){
-	console.log(req.status);
-	var status = "Your request is InProcess";
-	res.render('pages/status', {status:status});
+  var status = 'Your request is InProcess';
+  res.render('pages/status', {status:status});
 });
 
 // Template route
@@ -41,35 +56,10 @@ app.get('/createObj', function(req, res){
   res.render('pages/template');
 });
 
-// SlideShow route
-app.get('/slideshow', function(req, res){
 
-  var imgdata = {
+app.use('/prepareObject', require('./routes/prepare'));
 
-    img_1_responsive: "img/1-375.jpg 375, img/1-480.jpg 480, img/1.jpg 800",
-    img_1_src: "img/1-1600.jpg",
-    img_1_text: "Text Sample",
-    img_1_thumb: "img/thumb-1.jpg",
-
-    img_2_responsive: "img/2-375.jpg 375, img/2-480.jpg 480, img/2.jpg 800",
-    img_2_src: "img/2-1600.jpg",
-    img_2_text: "Text Sample",
-    img_2_thumb: "img/thumb-2.jpg",
-
-    img_3_responsive: "img/13-375.jpg 375, img/13-480.jpg 480, img/13.jpg 800",
-    img_3_src: "img/13-1600.jpg",
-    img_3_text: "Text Sample",
-    img_3_thumb: "img/thumb-13.jpg",
-
-    img_4_responsive: "img/4-375.jpg 375, img/4-480.jpg 480, img/4.jpg 800",
-    img_4_src: "img/4-1600.jpg",
-    img_4_text: "Text Sample",
-    img_4_thumb: "img/thumb-4.jpg",
-
-  }
-
-  res.render('pages/slideshow', {data: imgdata});
-});
+app.use('/slideshow', require('./routes/slideshow'));
 
 // server creation
 var server = app.listen(3000, function(){
@@ -77,5 +67,5 @@ var server = app.listen(3000, function(){
   var host = 'localhost';
   var port = server.address().port;
 
-  console.log("Listening at http://%s:%s",host,port);
+  console.log('Listening at http://%s:%s',host,port);
 });
